cb3e95e57e9f1a92272aa55629f6de827a7e5c3d
Moonkey <zhangguanhua1997@163.com>
2019-11-11 16:05:56 +0800

    修改生成文档的python脚本。

--- docs/GenDoc.py
+++ docs/GenDoc.py
@@ -1,7 +1,7 @@
 import os
 import re
 import subprocess
-
+import time
 
 current_dir = os.path.abspath(os.path.dirname(__file__)) + "/"
 
@@ -10,12 +10,11 @@ file_dict = {}
 file_list = ""
 with open(current_dir + "FileList", 'r') as flist:
     for f in flist.readlines():
-        if(f.endswith('\n')):
+        if (f.endswith('\n')):
             f = f[:-1]
         if re.findall(r"[.cpp|.hpp|.c|.h|.cxx|.hxx]$", f):
             file_dict[f] = ""
             print(f)
-
 
 # Traverse workspace to find related files in FileList
 for dirpath, dirnames, filenames in os.walk(current_dir + "/../"):
@@ -29,12 +28,42 @@ for k, v in file_dict.items():
     file_list += v + " "
 
 # Modify doxygen config
+print("Phase 1: " + "Modify configuration")
 prefix = "INPUT   = "
 cmd = "sed -i \'774c " + prefix + file_list + "\' " + current_dir + "Doxyfile"
-subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+config_result = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+while subprocess.Popen.poll(config_result) is None:
+    time.sleep(0.1)
 
-# Generate docs to html
+# Generate docs to html and latex
+print("Phase 2: " + "Generate docs")
 gen_doxygen_cmd = "doxygen " + current_dir + "Doxyfile"
 gen_result = subprocess.Popen(gen_doxygen_cmd, stdout=subprocess.PIPE, shell=True)
-out, err = gen_result.communicate()
-print(out)
+while subprocess.Popen.poll(gen_result) is None:
+    time.sleep(0.1)
+
+# Generate PDF in Chinese
+print("Phase 3: " + "Set utf-8 for Chinese pdf")
+replace_cmd = r"sed -i -e 's,begin{document},usepackage{CJKutf8}\n\\begin{document}\n\\begin{CJK}{UTF8}{gbsn},' " + current_dir + r"/latex/refman.tex"
+print(replace_cmd)
+replace_result = subprocess.Popen(replace_cmd, stdout=subprocess.PIPE, shell=True)
+while subprocess.Popen.poll(replace_result) is None:
+    time.sleep(0.1)
+
+
+replace_cmd = r"sed -i -e 's,end{document},end{CJK}\n\\end{document},' " + current_dir + r"/latex/refman.tex"
+print(replace_cmd)
+replace_result = subprocess.Popen(replace_cmd, stdout=subprocess.PIPE, shell=True)
+while subprocess.Popen.poll(replace_result) is None:
+    time.sleep(0.1)
+
+# Sleep until the replace operation finish
+print("Phase 4: " + "Transform latex to pdf")
+make_cmd = r"cd " + current_dir + r"/latex/;" + r"make -k"
+print(make_cmd)
+make_result = subprocess.Popen(make_cmd, stdout=subprocess.PIPE, shell=True)
+out, err = make_result.communicate()
+while subprocess.Popen.poll(make_result) is None:
+    print("Not finished")
+    time.sleep(0.1)
+
